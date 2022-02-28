d4562644c6f712aa121d1bd1c41f95b04b571884
ecourse <smkang@codenuri.co.kr>
2022-02-28 10:32:21 +0900

    Add files via upload

--- DAY1/1_namespace3.cpp
+++ DAY1/1_namespace3.cpp
@@ -1,9 +1,16 @@
 // 1_namespace3.cpp - 9page 내용
 
-#include <algorithm>  // C++ 표준 함수인 이헤더에 min 이 있습니다.
+// 1. 모든 C++ 표준은 std 라는 이름공간안에 있습니다.
 
+#include <algorithm>  // C++ 표준 함수인 이헤더에 min 이 있습니다.
+using namespace std;
+
+int count = 10;
 
 int main()
 {
-	int n1 = min(3, 4); // 3
+	int n1 = min(3, 4); // error
+	int n2 = std::min(3, 4); // ok
+
+	int n3 = count; // 이 코드가 왜??? 에러 일까요 ?
 }
