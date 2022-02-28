099f37d6e5c514941f78ab2076608e2a666c853a
ecourse <smkang@codenuri.co.kr>
2022-02-28 11:22:01 +0900

    Add files via upload

---
+++ DAY1/3_변수1.cpp
@@ -0,0 +1,17 @@
+// 3_변수1 - 18 page
+#include <iostream>
+
+// 변수와 타입은 "C" 언어에도 있는 개념이지만, C 의 기존 문법외에
+// C++에서 추가한 문법이 있습니다.
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+int main()
+{
+	int n1 = 10;
+	int n2 = 1000000;
+}
