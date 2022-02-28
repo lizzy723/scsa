0f4a87f7f1b1fa17c5c1463a41da81fb0218de33
ecourse <smkang@codenuri.co.kr>
2022-02-28 17:10:02 +0900

    Add files via upload

--- DAY1/9_레퍼런스2.cpp
+++ DAY1/9_레퍼런스2.cpp
@@ -9,11 +9,36 @@ int main()
 {
 	int a = 10, b = 10, c = 10;
 
-	inc1(a);
-	inc2(&b);
-	inc3(c ? &c);
+	inc1(a); // call by value, 복사본이 증가, 
+			 // 원본 a 는 증가 안됨. 실패
 
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << c << std::endl;
+	inc2(&b);// call by pointer
+			 // 원본 b 증가. 성공!
+	
+	inc3(c); // call by reference
+			 // 원본 c 증가. 성공 !
+
+	std::cout << a << std::endl; // 10
+	std::cout << b << std::endl; // 11
+	std::cout << c << std::endl; // 11
 }
+// 포인터와 레퍼런스는 유사한 특징이 있습니다.
+// 함수 인자로 사용시 모두 원본을 수정할수 있습니다.
+
+// 그런데, 레퍼런스가 쉽고, 안전합니다.
+
+void f1(int* p)
+{
+	// 포인터 사용시 안전한 코드로 작성하려면
+	if (p != 0) // 이렇게 해야 합니다.
+	{
+		*p = 10;
+	}
+}
+f1(0); // 이 코드가 가능합니다.
+
+void f2(int& r)
+{
+	r = 10;  // 2.그래서 이 코드는 항상 안전합니다.
+}
+f2(0); // error. 1. 반드시 변수를 보내야 합니다.!!
