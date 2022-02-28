cabff6fb6ebb6d22e9c6df6c7cec18ec2ee3f568
ecourse <smkang@codenuri.co.kr>
2022-02-28 10:34:13 +0900

    Add files via upload

--- DAY1/1_namespace3.cpp
+++ DAY1/1_namespace3.cpp
@@ -13,4 +13,7 @@ int main()
 	int n2 = std::min(3, 4); // ok
 
 	int n3 = count; // 이 코드가 왜??? 에러 일까요 ?
+					// C++ 표준에 "std::count()" 가 있습니다.
+					// using namespace std 때문에 
+					// 전역변수 count 와 표준 count()함수이름이 충돌 입니다
 }
