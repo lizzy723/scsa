64e0b95344f658b177b5b0f31fdb09644b70297b
ecourse <smkang@codenuri.co.kr>
2022-02-28 10:16:19 +0900

    Add files via upload

--- DAY1/1_namespace1.cpp
+++ DAY1/1_namespace1.cpp
@@ -1,6 +1,8 @@
 // 1_namespace1.cpp   -  7page
 #include <stdio.h>
-
+// 장점 : 1. 프로그램을 관련된 요소끼리 논리적으로 분리할수 있다.
+//        2. 이름 충돌을 막을수 있다. 
+//        C 언어를 제외한 대부분의 언어가 지원
 namespace Audio
 {
 	void init()
