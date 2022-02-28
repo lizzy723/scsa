89913eff48ab24a85915de65fccf63f19bf214c5
ecourse <smkang@codenuri.co.kr>
2022-02-28 10:35:25 +0900

    Add files via upload

--- DAY1/1_namespace3.cpp
+++ DAY1/1_namespace3.cpp
@@ -3,13 +3,13 @@
 // 1. 모든 C++ 표준은 std 라는 이름공간안에 있습니다.
 
 #include <algorithm>  // C++ 표준 함수인 이헤더에 min 이 있습니다.
-using namespace std;
+//using namespace std;
 
 int count = 10;
 
 int main()
 {
-	int n1 = min(3, 4); // error
+//	int n1 = min(3, 4); // error
 	int n2 = std::min(3, 4); // ok
 
 	int n3 = count; // 이 코드가 왜??? 에러 일까요 ?
---
+++ DAY1/Hello.cpp
@@ -0,0 +1,15 @@
+// 확장자 - 5 page 아래 부분
+// C언어 : .c
+// C++  :  .C, .cc, .cxx, .cpp
+
+// Hello.cpp
+
+#include <stdio.h>
+
+int main()
+{
+	printf("hello\n");
+}
+
+// 빌드하는법 : Ctrl + shift + B
+// 빌드 + 실행 : Ctrl + F5     => 강사는 이것 사용합니다.
