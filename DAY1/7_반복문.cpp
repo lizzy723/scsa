9c97492d85b2ca9dc3bbf247548653dc2b2f12aa
ecourse <smkang@codenuri.co.kr>
2022-02-28 16:23:22 +0900

    Add files via upload

--- DAY1/7_반복문.cpp
+++ DAY1/7_반복문.cpp
@@ -1,7 +1,6 @@
 // 7_반복문    38page
-
 #include <iostream>
-
+#include <string>
 int main()
 {
 	int x[5] = { 1,2,3,4,5 };
@@ -10,10 +9,24 @@ int main()
 	for (int i = 0; i < 5; i++)
 	{
 	}
-
 	// 새로운 for 문 ( range-for, C++11)
-	for (int e : x)
-	{
+	// 1. 전체 열거가 필요할때만 사용하세요
+	// 2. 일부 열거나, +=2 로 이동하려면 위에 기존 for문 사용
+//	for (int e : x)
+	for (auto e : x) // x는 배열뿐아니라 C++표준의 list, vector 등도모두
+	{					// 사용가능합니다.
 		std::cout << e << ", ";
 	}
+
+	std::string s = "abcdefg";
+	for (auto c : s)
+	{
+		std::cout << c << ", ";
+	}
+
+	// 동적할당된 배열도 되나요 ??? => 안됩니다.
+//	int* arr = (int*)malloc(sizeof(int) * 10); // arr는 range-for 안됩니다.
 }
+
+
+
