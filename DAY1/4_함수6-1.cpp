7e28ac815bdc31b843de66d09c9c122596c5ae58
ecourse <smkang@codenuri.co.kr>
2022-02-28 15:44:31 +0900

    Add files via upload

--- DAY1/4_함수6-1.cpp
+++ DAY1/4_함수6-1.cpp
@@ -1,7 +1,14 @@
 // 4_함수6-1
 #include <iostream>
 
+/*
 int add(int a, int b)
+{
+	return a + b;
+}
+*/
+template<typename T>
+T add(T a, T b)
 {
 	return a + b;
 }
@@ -9,6 +16,13 @@ int add(int a, int b)
 int main()
 {
 	std::cout << add(1,   2)   << std::endl;
-	std::cout << add(1.1, 2.2) << std::endl;
+	std::cout << add(1.1, 2.2) << std::endl; 
+
+//	std::cout << add(1, 2.2) << std::endl; // 템플릿 인자는 T, T 인데
+									// 1, 2번째 인자의 타입이 다르므로
+									// 컴파일러가 T 타입 결정 못함. error
+
+	// 아래 처럼 타입 인자를 전달하면 에러 아님
+	std::cout << add<double>(1, 2.2) << std::endl;
 }
 
