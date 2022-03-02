b137ac2978e45daf89d52d396b8a3294ce07b4e2
ecourse <smkang@codenuri.co.kr>
2022-03-02 09:19:53 +0900

    Add files via upload

--- DAY2/2_nullptr2.cpp
+++ DAY2/2_nullptr2.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-void fn(int  n) { std::cout << "int" << std::endl; }
-void fn(int* p) { std::cout << "int*" << std::endl; }
+void fn(int    n) { std::cout << "int" << std::endl; }
+void fn(double n) { std::cout << "double" << std::endl; }
+void fn(bool   n) { std::cout << "bool" << std::endl; }
+void fn(int*   p) { std::cout << "int*" << std::endl; }
 
 int main()
 {
