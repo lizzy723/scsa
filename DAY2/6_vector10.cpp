753c217260429b4cf1b31b2afd2018ed76da6c00
ecourse <smkang@codenuri.co.kr>
2022-03-02 12:44:20 +0900

    Add files via upload

--- DAY2/6_vector10.cpp
+++ DAY2/6_vector10.cpp
@@ -9,11 +9,11 @@ int main()
 	while (1)
 	{
 		std::cin >> n;
-
 		if (n == -1) break;
 
 		v.push_back(n); // 자동으로 크기 증가 합니다.
 	}
+
 	std::cout << "입력된 갯수 : " << v.size() << std::endl;
 
 	// range-for 에 STL 컨테이너 넣을수 있습니다.
@@ -23,6 +23,12 @@ int main()
 	// vector v 파괴시 소멸자에서 동적 할당된 메모리 파괴해 줍니다.
 }
 
+// C++을 배우는 것은 !!
 
+// 1. vector 같은 타입을 만드는 문법을 배우는것 - 어렵습니다.!!(초~중급)
+
+// 2. std::vector 같은 이미 만들어진 타입(클래스)의 사용법을 배우는 것 => 쉽습니다.
+
+// C++ 표준 라이브러리 : std::vector, std::string, std::list 등....
 
 
