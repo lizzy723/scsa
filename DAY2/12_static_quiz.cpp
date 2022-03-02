e4e426044473f81e48b1485895e9af6e7c9a00e4
ecourse <smkang@codenuri.co.kr>
2022-03-02 17:30:38 +0900

    Add files via upload

--- DAY2/12_static_quiz.cpp
+++ DAY2/12_static_quiz.cpp
@@ -5,20 +5,24 @@ class Object
 	static int sdata;
 public:
 	// 다음중 에러를 모두 고르세요
+	// static 멤버 데이타 : 객체가 없어도 메모리에 있고, 모든 객체가 공유
+	// static 멤버 함수   : 객체없이 호출가능
 	void f1()
 	{
-		idata = 0;	// 1
-		sdata = 0;	// 2
-		f2();		// 3
+		idata = 0;	// 1. ok
+		sdata = 0;	// 2. ok
+		f2();		// 3. ok
 	}
-	void f2()
+	// static 멤버 함수는 static 멤버만 접근 가능합니다
+	static void f2()
 	{
-		idata = 0;	// 4
-		sdata = 0;  // 5
-		f1();		// 6
+		idata = 0;	// 4. error
+		sdata = 0;  // 5. ok
+		f1();		// 6. error
 	}
 };
 int Object::sdata = 0;
 int main()
 {
+	Object::f2();
 }
