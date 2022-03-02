43064fd7deef557c611eb342cd3b2099d69e9bf9
ecourse <smkang@codenuri.co.kr>
2022-03-02 15:31:46 +0900

    Add files via upload

---
+++ DAY2/9_필드초기화1.cpp
@@ -0,0 +1,15 @@
+// 9_필드초기화1.cpp
+class Point
+{
+	// C++11 부터는 멤버 데이타를 아래처럼 직접 초기화 할수 있습니다.
+	int x = 0;
+	int y{ 0 }; // 이렇게해도 됩니다.
+public:
+	Point() {}
+	Point(int a)        : x(a) {}
+	Point(int a, int b) : x(a), y(b) {}
+};
+int main()
+{
+	Point pt;
+}
