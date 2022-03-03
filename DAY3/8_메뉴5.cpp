2c341448038a3594cf72eb00dee7878a9e247aaf
ecourse <smkang@codenuri.co.kr>
2022-03-03 16:16:40 +0900

    Add files via upload

--- DAY3/8_메뉴5.cpp
+++ DAY3/8_메뉴5.cpp
@@ -76,7 +76,9 @@ int main()
 	PopupMenu* pm1 = new PopupMenu("색상 변경");
 	PopupMenu* pm2 = new PopupMenu("해상도 변경");
 	root->addMenu(pm1);
-	root->addMenu(pm2);
+//	root->addMenu(pm2);
+	pm1->addMenu(pm2);
+
 	pm1->addMenu(new MenuItem("RED",   11));
 	pm1->addMenu(new MenuItem("GREEN", 12));
 	pm1->addMenu(new MenuItem("BLUE",  13));
@@ -86,10 +88,19 @@ int main()
 	pm2->addMenu(new MenuItem("UHD", 23));
 
 	// 이제 시작하려면 ??
-	? ;
+	root->command();
 }
 // 객체지향 언어 에서 프로그램이란?
 // 객체를 생성하고, 객체간의 관계를 설정하고, 
 // 객체간에 메세지를 주고 받는 것이다 <<== 멤버 함수를 호출한다는 의미
+// ====> "객체(Object) 지향(Oriented) 프로그래밍(Programming) => OOP"
 
+// 위와 같은 설계 방법을 배우는 수업 - "디자인 패턴"을 공부해 보세요
+// 위 코드가 "Composite 패턴"이라는 디자인 기법입니다.
+
+// 복합 객체(PoupuMenu)는 개별객체(MenuItem) 뿐 아니라 
+// 복합 객체 자신도 보관할수 있다.
+// => 복합객체와 개별객체는 공통의 기반 클래스가 필요 하다.
+
+// Folder는 File 뿐아니라 Folder 자체 도 보관한다.
 
