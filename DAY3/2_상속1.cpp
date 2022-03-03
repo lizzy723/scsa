50f2d6e8b3396acdfc562a9a0c856a0ff72d8c8c
ecourse <smkang@codenuri.co.kr>
2022-03-03 09:45:54 +0900

    Add files via upload

--- DAY3/2_상속1.cpp
+++ DAY3/2_상속1.cpp
@@ -2,18 +2,33 @@
 #include <iostream>
 #include <string>
 
-class Student
+// 모든 인간의 공통의 특징을 설계
+class Person
 {
 	std::string name;
 	int age;
+};
+
+// 상속 : 기존 타입(클래스)을 확장해서 새로운 타입(클래스)을 만드는 문법
+// C++    : class Student : public Person 
+// C#     : class Student : Person 
+// python : class Student(Person)
+// java   : class Student extends Person 
+
+// Person  : Base(기반) class,    Super Class, Parent Class
+// Student : Derived(파생) class, Sub Class,   Child Class
+// 133 그림도 알아 두세요
+
+class Student : public Person 
+{
 	int id;
 };
-class Professor
+
+class Professor : public Person
 {
-	std::string name;
-	int age;
 	int major;
 };
+
 int main()
 {
 	Student   s;
