4fd67dc2288c4b990e8dd19a2c4b98bfca9efb09
ecourse <smkang@codenuri.co.kr>
2022-03-03 09:36:35 +0900

    Add files via upload

---
+++ DAY3/2_상속1.cpp
@@ -0,0 +1,21 @@
+// 2_»ó¼Ó1 - 132page
+#include <iostream>
+#include <string>
+
+class Student
+{
+	std::string name;
+	int age;
+	int id;
+};
+class Professor
+{
+	std::string name;
+	int age;
+	int major;
+};
+int main()
+{
+	Student   s;
+	Professor p;
+}
