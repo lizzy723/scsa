3171e9655a499df84f2a5378af7f754e75973821
ecourse <smkang@codenuri.co.kr>
2022-03-03 09:34:29 +0900

    Add files via upload

---
+++ DAY3/정리1.cpp
@@ -0,0 +1,20 @@
+// 정리1.
+
+// class 문법 정리
+// 1. 접근 지정자 : private vs public, 캡슐화(encapsulation)
+
+// 2. 생성자, 소멸자
+
+// 3. 멤버 초기화 리스트 : Point(int a, int b) : x(a), y(b) {}
+
+// 4. static member data
+//    static member function   ==> Date 클래스 예제
+
+// 5. const member function    ==> 상수 객체는 상수 멤버 함수만 호출가능
+//								   모든 getter 는 상수 멤버 함수로 만들어야 한다.
+
+// 6. this ==> 멤버 함수의 호출 원리   p1.set(10, 20) ==> set(&p1, 10, 20)
+
+// 7. 복사 생성자- 내일
+//--------------------------------------------------------------
+// 상속!! 
