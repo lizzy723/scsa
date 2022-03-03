4957a0802bac47548532eb353463efbbddf9e7e0
ecourse <smkang@codenuri.co.kr>
2022-03-03 17:06:34 +0900

    Add files via upload

--- DAY3/10_복사생성자2.cpp
+++ DAY3/10_복사생성자2.cpp
@@ -8,7 +8,11 @@ class Person
 public:
 	Person(const char* s, int a) : age(a)
 	{
+		name = new char[strlen(s) + 1];
+
+		strcpy_s(name, strlen(s) + 1, s); // strcpy 의 보안 강화 버전
 	}
+	~Person() { delete[] name; }
 };
 
 int main()
