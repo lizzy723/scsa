35858cd33de9fcc377cc1a3c1deb358dd27a078e
ecourse <smkang@codenuri.co.kr>
2022-03-03 10:11:21 +0900

    Add files via upload

---
+++ DAY3/4_상속과생성자1.cpp
@@ -0,0 +1,25 @@
+// 4_상속과생성자1 - 135page
+
+#include <iostream>
+
+class Base
+{
+	int data;
+public:
+	Base()      { std::cout << "Base()" << std::endl; }
+	Base(int a) { std::cout << "Base(int)" << std::endl; }
+	~Base()     { std::cout << "~Base()" << std::endl; }
+};
+
+class Derived : public Base
+{
+public:
+	Derived()      { std::cout << "Derived()" << std::endl; }
+	Derived(int a) { std::cout << "Derived(int)" << std::endl; }
+	~Derived()     { std::cout << "~Derived()" << std::endl; }
+};
+
+int main()
+{
+	Derived d1; // 이순간 결과를 예측해 보세요
+}
