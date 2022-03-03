3bfacb91a1200e0f3d937ef45b18d6a6d46269f3
ecourse <smkang@codenuri.co.kr>
2022-03-03 16:40:11 +0900

    Add files via upload

--- DAY3/10_복사생성자1.cpp
+++ DAY3/10_복사생성자1.cpp
@@ -6,14 +6,24 @@ class Point
 	int x;
 	int y;
 public:
-	Point()             : x(0), y(0) {}
-	Point(int a, int b) : x(a), y(b) {}
+	Point()             : x(0), y(0) {} // 1
+	Point(int a, int b) : x(a), y(b) {} // 2
+
+	// 복사 생성자 : 자신과 동일한 타입의 객체 한개를 인자로 가지는 생성자
+	//			    사용자가 만들지 않으면 컴파일러가 제공
+	// 컴파일러가 제공하는 것은 아래와 동일
+	Point(const Point& p) : x(p.x), y(p.y)
+	{
+	}
 };
 int main()
 {
 	// 아래 4줄을 생각해 보세요
-	Point p1;
-	Point p2(1, 2);
-	Point p3(1);
-	Point p4(p2);
+	Point p1;		// ok.. 1번 생성자
+	Point p2(1, 2);	// ok.. 2번 생성자
+//	Point p3(1);	// error. Point(int) 모양의 생성자는 없다!
+	
+	Point p4(p2);	// ok.    Point(Point) 모양의 생성자 필요!
+
+	Point p5 = p2;  // ok.. 위와 동일
 }
