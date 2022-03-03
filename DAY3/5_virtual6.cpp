7d37096661419bd37e575f91f562b8aeba9603f9
ecourse <smkang@codenuri.co.kr>
2022-03-03 14:27:35 +0900

    Add files via upload

--- DAY3/5_virtual6.cpp
+++ DAY3/5_virtual6.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+// 149 page
+
+// 핵심 : 기반 클래스로 사용되는 클래스를 만들때는
+//		반드시 소멸자를 가상함수로 만들어야 한다.
 
 class Base
 {
 public:
 	Base() {}
-	~Base() { }
+	virtual ~Base() {} // 가상 소멸자
 };
 class Derived : public Base
 {
@@ -14,5 +18,14 @@ public:
 };
 int main()
 {
-	Derived d;
+//	Derived d;
+
+	Base* p = new Derived;	// 1. 메모리 할당
+							// 2. 생성자 호출 - Derived()
+
+	delete p;	// 1. 소멸자 호출
+				//     A. p의 타입을 보고 소멸자 선택하면 => ~Base()
+				//     B. p가 가리키는 메모리를 조사해서 소멸자를 선택하면 => ~Derived()
+
+				// 2. p가 가리키는 메모리 해지
 }
