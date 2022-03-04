c7a6fedca5dfa46618fbe727ba363250c525a834
ecourse <smkang@codenuri.co.kr>
2022-03-04 12:03:01 +0900

    Add files via upload

---
+++ DAY4/5_STL_CONTAINER2.cpp
@@ -0,0 +1,15 @@
+// 5_STL_CONTAINER2
+#include <iostream>
+
+// 선형 컨테이너
+#include <vector>
+#include <list>
+#include <deque>
+
+#include <set>
+#include <unordered_set>
+
+int main()
+{
+	std::set<int> s = { 50, 30, 70, 40, 80, 60, 35 };
+}
