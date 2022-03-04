742847abd1793bbf725e6ae402cf5deeebe5960f
ecourse <smkang@codenuri.co.kr>
2022-03-04 11:35:51 +0900

    Add files via upload

---
+++ DAY4/5_STL_CONTAINER1.cpp
@@ -0,0 +1,16 @@
+// 5_STL_CONTAINER1 - 214 page
+#include <iostream>
+
+// 선형 컨테이너
+#include <vector>
+#include <list>
+#include <deque>
+
+int main()
+{
+	int x[5] = { 1,2,3,4,5 };
+
+	std::vector<int> c1 = { 1,2,3,4,5 };
+	std::list<int>   c2 = { 1,2,3,4,5 };
+	std::deque<int>  c3 = { 1,2,3,4,5 };
+}
