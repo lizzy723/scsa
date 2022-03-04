c31f75f0428028a586f7c1688992ed64d287510b
ecourse <smkang@codenuri.co.kr>
2022-03-04 16:07:28 +0900

    Add files via upload

--- DAY4/9_map1.cpp
+++ DAY4/9_map1.cpp
@@ -9,11 +9,17 @@ int main()
 	// set : 한개의 데이타(key)를 저장
 	std::set<std::string> s = { "mon", "wed", "sun" };
 
-	// map : 2개의 데이타쌍(key-value) 저장
+
+
+	// map : 2개의 데이타쌍(key-value) 저장하는 set(tree)
 	std::map<std::string, std::string> m = { {"mon","월요일"}, {"sun", "일요일"} };
 
 	//[] 도 사용가능
 	m["sat"] = "토요일";
 
-	std::cout << s["sun"] << std::endl;
+	std::cout << m["sun"] << std::endl;
+		
+	// C++ : map 이라고 합니다.
+	// C#, python, java 등에서는 "dictionary" 라고 합니다.
+	//							파이썬 : dict
 }
