4b0c63fdc734c1a60e8aa2e32e79d4dd8510ecf4
ecourse <smkang@codenuri.co.kr>
2022-03-04 11:19:13 +0900

    Add files via upload

--- DAY4/4_reference_return3.cpp
+++ DAY4/4_reference_return3.cpp
@@ -4,18 +4,26 @@ class Counter
 {
 	int cnt = 0;
 public:
-	void increment()
+//	Counter  increment() // 절대 값 반환 하면 안됩니다.!! 복사본 객체 계속 생성됩니다.
+	Counter& increment() // 이렇게 해야 합니다.
 	{
 		++cnt;
+		return *this;
 	}
 	int getCount() const { return cnt; }
+
+	~Counter() { std::cout << "~Counter" << std::endl; }
 };
 
 int main()
 {
 	Counter c;
-	c.increment();
-	c.increment();
-	c.increment();
-	std::cout << c.getCount() << std::endl;
+//	c.increment();
+//	c.increment();
+//	c.increment();
+	std::cout << "---------" << std::endl;
+	c.increment().increment().increment();
+	std::cout << "---------" << std::endl;
+
+	std::cout << c.getCount() << std::endl; // ???
 }
