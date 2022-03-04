563d6d1ce4569009772c8a9145cc4c24abfcdd48
ecourse <smkang@codenuri.co.kr>
2022-03-04 15:38:32 +0900

    Add files via upload

---
+++ DAY4/8_STL정리.cpp
@@ -0,0 +1,15 @@
+// 8_STL정리
+
+// 1. 프로그램에서 다루고자 하는 데이타의 형태(용도)에 따라 컨테이너(자료구조 선택하세요)
+//    선형 : list, vector, deque, stack, queue
+//   비선형 : set, unordered_set, map, unordered_map 
+//   에서 선택하세요.
+
+// 2. 컨테이너에 저장된 데이타에 어떤 연산을 하고 싶으면
+//    1. "STL 알고리즘" 에서 적합한 함수가 있는지 찾으세요..
+//    2. 있으면 사용하고, 
+//    3. 없으면 직접 구현하세요.
+
+// 3. STL 의 3대 요소 : Container, Iterator, Algorithm
+
+// cppreference.com 에 접속해 보세요..
